d38856161c16e6868e9f7f1073ddfc5bc7eaf84e
BilousVladyslav <mytwink.steam@gmail.com>
2020-05-10 11:19:06 +0300

    configured urls and add STATICs to settings

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from api.payment.views import PayView, PayCallbackView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^pay/$', PayView.as_view(), name='pay_view'),
+    url(r'^pay-callback/$', PayCallbackView.as_view(), name='pay_callback')
 ]
